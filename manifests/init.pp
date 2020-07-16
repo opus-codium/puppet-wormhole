423faa0b06b7e87a51e32cf467873e15ad293389
Romain Tartière <romain@blogreen.org>
2020-07-15 15:52:11 -1000

    Add doc

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,8 @@
+# @summary Manage magic-wormhole
+#
+# @param manage_package Whether the wormhole package should be managed by the module
+# @param package_name Name of the wormhole package
+# @param package_ensure Ensure value for the wormhole package resource
 class wormhole (
   Boolean $manage_package = true,
   String  $package_name   = 'magic-wormhole',
--- manifests/package.pp
+++ manifests/package.pp
@@ -1,4 +1,6 @@
 class wormhole::package {
+  assert_private()
+
   if $wormhole::manage_package {
     package { $wormhole::package_name:
       ensure => $wormhole::package_ensure,
